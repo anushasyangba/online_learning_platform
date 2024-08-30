1481235c650b5fb70922d876e932a3101c3519b5
anusha <anushatamang554@gmail.com>
2024-08-30 18:52:52 +0545

    change fifth

--- SAcademy/main/models.py
+++ SAcademy/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    category = models.CharField(max_length=100)
+    instructor = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='course_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    includes = models.JSONField(default=list)  # ['Syllabus', 'Videos', 'Quizzes', ...]
+
+    def __str__(self):
+        return self.title
