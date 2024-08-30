946ffc8d254ab1d7a82483cad1571c4cc481f15c
anusha <anushatamang554@gmail.com>
2024-08-30 14:30:48 +0545

    change fourth

--- SAcademy/main/urls.py
+++ SAcademy/main/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
 from .views import *
+
+urlpatterns = [
+    path("",home,name="home"),
+    path("detail_course/",detail_course,name="detail_course"),
+]
--- SAcademy/main/views.py
+++ SAcademy/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+def detail_course(request):
+    return render(request,'detail_course.html')
